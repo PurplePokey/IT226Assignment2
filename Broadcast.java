dd21b05850e0cc4efab1f79247e591882e729108
PurplePokey <46290939+PurplePokey@users.noreply.github.com>
2019-12-03 10:30:52 -0600

    attempted merge, bugs still present

---
+++ Broadcast.java
@@ -0,0 +1,25 @@
+package com.example.androidalarm;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Broadcast extends BroadcastReceiver {
+
+    MainActivity main = new MainActivity();
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Date currentTime = Calendar.getInstance().getTime();
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+        Toast.makeText(context, formatter.format(currentTime) + " Message: " , Toast.LENGTH_LONG).show();
+    }
+
+}
